19d83fcab92046553f9569c8210e59b85c9255de
epumeriribarren <informatico@barcelonaled.com>
2022-10-13 11:21:36 +0000

    [UPD] update for odoo v15

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,1 @@
-
-{
-    'name': 'Copiar notas de pedido en factura',
-    'version': '12.0.1.0.0',
-    'category': 'Sales',
-    'author': 'Epumer Pehuen Iribarren Inchausti',
-    'summary': 'Copia las notas del pedido en la factura',
-    'depends': [
-        'sale_management',
-        'sale_stock'
-    ],
-    'installable': True,
-    'auto_install': False,
-}
+{'name': 'Copiar notas de pedido en factura', 'version': '12.0.1.0.0', 'category': 'Sales', 'author': 'Epumer Pehuen Iribarren Inchausti', 'summary': 'Copia las notas del pedido en la factura', 'depends': ['sale_management', 'sale_stock'], 'installable': True, 'auto_install': False, 'data': []}
